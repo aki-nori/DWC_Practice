bfb4cf2b8b8e877e4fa853cdea4f7b6344a5de4a
aki-nori <i.akinori09@gmail.com>
2020-01-11 12:29:49 +0000

    [Finish]フォロー機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   end
 
   def index
-  	@users = User.all #一覧表示するためにUserモデルのデータを全て変数に入れて取り出す。
+  	@users = User.all #一覧表示するためにUserモデルのデータを全  て変数に入れて取り出す。
   	@book = Book.new #new bookの新規投稿で必要（保存処理はbookコントローラー側で実施）
   end
 
@@ -28,6 +28,14 @@ class UsersController < ApplicationController
   	end
   end
 
+  def follows
+    @users = User.find(params[:id]).followers
+  end
+
+  def followers
+    @users = User.find(params[:id]).followings
+  end
+
   private
 
   def user_params
@@ -40,5 +48,4 @@ class UsersController < ApplicationController
   		redirect_to user_path(current_user)
   	end
   end
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,14 @@ class User < ApplicationRecord
   # 当該UserがコメントしたBooksを取得できる
   has_many :book_commented_books, through: :book_coments, source: :book
 
+  # 自分がフォローしているユーザとの関連
+  has_many :active_relationships, class_name: "Relationship", foreign_key: :following_id
+  has_many :followings, through: :active_relationships, source: :follower
+
+  # 自分がフォローされているユーザとの関連
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id
+  has_many :followers, through: :passive_relationships, source: :following
+
   attachment :profile_image, destroy: false
 
   #バリデーションは該当するモデルに設定する。エラーにする条件を設定できる。
